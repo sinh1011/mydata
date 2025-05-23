549da1c50825b52eaef39c3bb0f238a1159bdefc
Sinh <sinhtruongvo@gmail.com>
2025-05-23 19:01:08 +0700

    up lan dau

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// let ischeck = true
+// if (ischeck) {
+//     console.log("Dang nhap thanh cong");
+// }
+// else{
+//     console.log("Dang nhap that bai");
+// }
+// console.log(ischeck)
+// console.log("sinh dep trai");
+const user = {
+  ten: "sinh",
+  tuoi: 32,
+};
+
+const products = [
+  {
+    name: "iphone 15 pro max",
+    oldprice: 21990,
+    saleprice: 20990,
+    amount: 200,
+    hotproduct: true,
+    img: "",
+  },
+  {
+    name: "iphone 16 pro max",
+    oldprice: 29990,
+    saleprice: 27990,
+    amount: 512,
+    hotproduct: true,
+    img: "",
+  },
+];
+
+console.log(products);
