f9c9c75fc81d5794adff35fbeb5d47a4477f41ec
Vo Truong Sinh <sinhtruongvo@gmail.com>
2025-08-29 02:03:18 +0700

    code chua day

---
+++ dom/main.js
@@ -0,0 +1,2 @@
+console.log([document.URL]);
+document.getElementById("vedeptrai").innerText="Truong Sinh"
---
+++ es6/main.js
@@ -0,0 +1,11 @@
+const MyFun = () => {
+  console.log("xin chao");
+};
+MyFun();
+
+let arr1 = [1, 2, 3, 4, 5];
+const double = arr1.map((item) => item * 2);
+console.log(double);
+
+console.log("=================");
+
