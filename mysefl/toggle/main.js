6e7a9eb239a71a80d5259cc05ad923d4f786c1d0
Vo Truong Sinh <sinhtruongvo@gmail.com>
2025-09-29 21:37:07 +0700

    upload

--- mysefl/toggle/main.js
+++ mysefl/toggle/main.js
@@ -6,6 +6,29 @@ const btn2 = document.getElementById("btn2");
 const btnDarkMode = document.getElementById("btnDarkMode");
 const btnMenu = document.getElementById("btnMenu");
 
+const title = document.getElementById("changeText");
+const btnDisplay = document.getElementById("btnDisplay");
+
+const count = document.getElementById("count");
+const btnClick = document.getElementById("btnClick");
+
+const fullName = document.getElementById("fullName");
+const btnShowName = document.getElementById("btnShowName");
+const showName = document.getElementById("showName");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 btnChange.addEventListener("click", function () {
   text.classList.toggle("red");
 });
@@ -18,6 +41,24 @@ btnDarkMode.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
 
- btnMenu.addEventListener("click", function () {
-    menu.classList.toggle("hidden");
-  });
+btnMenu.addEventListener("click", function () {
+  menu.classList.toggle("hidden");
+});
+
+btnDisplay.addEventListener("click", function () {
+  title.innerText = "Xin chào Sinh đẹp trai";
+});
+
+let counter = 0;
+
+btnClick.addEventListener("click", function () {
+  count.innerHTML = `
+  Bạn đã click ${++counter} lần`;
+});
+
+
+btnShowName.addEventListener("click", function () {
+showName.innerHTML = `<p>Tên của bạn là: ${(fullName.value)}</p>`
+console.log(fullName.value);
+});
+
