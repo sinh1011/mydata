768d3fbc95c83281f258b38018e16395e3a359bf
Vo Truong Sinh <sinhtruongvo@gmail.com>
2025-10-04 21:38:19 +0700

    up code

---
+++ promiss/main.js
@@ -0,0 +1,53 @@
+// console.log("a");
+
+// setTimeout(() => {
+//   console.log(3);
+// }, 0);
+
+// console.log(1);
+
+// setTimeout(() => {
+//   console.log(2);
+// }, 1000);
+
+// call api
+
+// const loadData = () => {
+//   fetch("https://dummyjson.com/products?limit=10")
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
+
+// loadData();
+const loader= document.getElementById("loader")
+const box = document.getElementById("box");
+
+const productsData = fetch("https://dummyjson.com/products?limit=12")
+  .then((res) => res.json())
+  .then((data) => data.products)
+  .then((products) => {
+    loader.classList.toggle("hidden")
+    console.log(products);
+    render(products);
+  })
+  .catch((err) => console.log(err));
+
+// console.log(productsData);
+
+const render = (arr = []) => {
+  box.innerHTML = arr
+    .map((item) => {
+      return `
+      <div class="box">
+          <h2>${item.title}</h2>
+          <p>brand:${item.price}</p>
+          <p>price:${item.price}</p>
+          <p>stock:${item.stock}</p>
+       </div>`;
+    })
+    .join("");
+};
