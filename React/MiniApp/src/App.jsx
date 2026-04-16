3441b1c4708bef22cc4fb66fea53da2875c2bafa
Võ Trường Sinh <sinhtruongvo@gmail.com>
2026-04-17 01:18:44 +0700

    upload

--- React/MiniApp/src/App.jsx
+++ React/MiniApp/src/App.jsx
@@ -13,14 +13,20 @@ function TodoApp() {
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("all");
 
   const handleAdd = (e) => {
     e.preventDefault();
     if (!input.trim()) return;
 
-    setTodoList((prev) => [...prev, { id: Date.now(), name: input }]);
+    setTodoList((prev) => [
+      ...prev,
+      { id: Date.now(), name: input, done: false },
+    ]);
     setInput("");
   };
+
+  console.log(todoList);
 
   const handleReset = () => {
     setTodoList([]);
@@ -32,9 +38,21 @@ function TodoApp() {
     setTodoList((prev) => prev.filter((i) => i.id !== id));
   };
 
-  const filteredTodos = todoList.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase()),
-  );
+  const handleToogle = (id) => {
+    setTodoList((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item,
+      ),
+    );
+  };
+
+  const filteredTodos = todoList.filter((item) => {
+    const filterSearch = item.name.toLowerCase().includes(search.toLowerCase());
+    const status =
+      filter === "all" ? true : filter === "done" ? item.done : !item.done;
+
+    return filterSearch && status;
+  });
 
   return (
     <>
@@ -45,9 +63,13 @@ function TodoApp() {
         handleReset={handleReset}
       />
 
-      <SearchBox search={search} setSearch={setSearch} />
+      <SearchBox search={search} setSearch={setSearch} setFilter={setFilter} />
 
-      <TodoDisplay filteredTodos={filteredTodos} handleDelete={handleDelete} />
+      <TodoDisplay
+        filteredTodos={filteredTodos}
+        handleDelete={handleDelete}
+        handleToogle={handleToogle}
+      />
     </>
   );
 }
@@ -87,16 +109,19 @@ function ResetButton({ handleReset }) {
   );
 }
 
-function SearchBox({ search, setSearch }) {
+function SearchBox({ search, setSearch, setFilter }) {
   return (
     <label>
       Search{" "}
       <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <button onClick={() => setFilter("all")}>All</button>
+      <button onClick={() => setFilter("active")}>Active</button>
+      <button onClick={() => setFilter("done")}>Done</button>
     </label>
   );
 }
 
-function TodoDisplay({ filteredTodos, handleDelete }) {
+function TodoDisplay({ filteredTodos, handleDelete, handleToogle }) {
   if (filteredTodos.length === 0) {
     return <p>Chưa có công việc</p>;
   }
@@ -105,7 +130,12 @@ function TodoDisplay({ filteredTodos, handleDelete }) {
     <>
       {filteredTodos.map((item) => (
         <div key={item.id}>
-          <p>- {item.name}</p>
+          <p
+            onClick={() => handleToogle(item.id)}
+            style={{ textDecoration: item.done ? "line-through" : "none" }}
+          >
+            {item.name}
+          </p>
           <button type="button" onClick={() => handleDelete(item.id)}>
             Delete
           </button>
