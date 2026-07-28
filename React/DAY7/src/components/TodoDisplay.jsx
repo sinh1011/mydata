e3ac000874cd588128b378e2c8221906785bca7a
Võ Trường Sinh <sinhtruongvo@gmail.com>
2026-07-28 22:19:22 +0700

    fullday7

--- React/DAY7/src/components/TodoDisplay.jsx
+++ React/DAY7/src/components/TodoDisplay.jsx
@@ -1,21 +1,23 @@
+import styles from "./TodoDisplay.module.css";
 export default function TodoDisplay({ list, handleDelete, toggleComplete }) {
   return (
     <>
       {list.length === 0 && <p>Chưa có công việc nào!</p>}
 
       {list.map((i) => (
-        <div key={i.id}>
+        <div key={i.id} className={styles.TodoItem}>
           <p
+            className={`${styles.TodoText} ${i.complete ? styles.complete : ""}`}
             onClick={() => toggleComplete(i.id)}
-            style={{
-              cursor: "pointer",
-              textDecoration: i.complete ? "line-through" : "none",
-              color: i.complete ? "#aaa" : "#000",
-            }}
           >
             - {i.name}
           </p>
-          <button onClick={() => handleDelete(i.id)}>Delete</button>
+          <button
+            className={`${styles.TodoButton}`}
+            onClick={() => handleDelete(i.id)}
+          >
+            Delete
+          </button>
         </div>
       ))}
     </>
--- React/DAY7/src/components/TodoSearch.jsx
+++ React/DAY7/src/components/TodoSearch.jsx
@@ -1,3 +1,4 @@
+import styles from "./TodoSearch.module.css";
 export default function TodoSearch({ search, setSearch }) {
   return (
     <>
@@ -5,6 +6,7 @@ export default function TodoSearch({ search, setSearch }) {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         placeholder="Nhập công việc cần tìm..."
+        className={styles.searchInput}
       ></input>
     </>
   );
